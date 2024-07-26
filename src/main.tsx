a7e3477002ccfd04f7795d1a0c4264645cb40f40
Giovani Carnaval <gi.carnaval91@gmail.com>
2024-07-26 18:51:48 -0300

    Changing styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="w-full h-dvh text-center flex justify-center items-center bg-main-background bg-cover">
+    <div className="w-full h-dvh text-center flex justify-center items-center bg-main-background bg-cover overflow-hidden">
       <App />
     </div>
   </React.StrictMode>,
